874f4fb273fbad5709c0daf8c201dec602404131
CristianPietro-7 <cristianpietrobon2019@gmail.com>
2024-12-01 20:48:11 -0300

    archivo javascript

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-hola = "pepe"
+hola = "pepe";
+palabra = "";
+
+longitud = hola.length
+
+if (longitud %2 == 0) {
+    console.log("La palabra es par")
+} else {
+    console.log("La palabra es impar")
+    
+}
